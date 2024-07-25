3b41ab7460a32395009348910afd2dfe73594e1b
Roy Sam <roy.sam@live.com>
2024-07-25 23:42:09 +0800

    Update utd-vuln-code-develop/terragoat-s3.tf
    Co-authored-by: prisma-cloud-devsecops[bot] <89982750+prisma-cloud-devsecops[bot]@users.noreply.github.com>

--- utd-vuln-code-develop/terragoat-s3.tf
+++ utd-vuln-code-develop/terragoat-s3.tf
@@ -25,6 +25,57 @@ resource "aws_s3_bucket" "data_log_bucket" {
   bucket = "data-log-bucket"
 }
 
+
+resource "aws_s3_bucket_versioning" "data_log_bucket" {
+  bucket = aws_s3_bucket.data_log_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket" "data_log_bucket_destination" {
+  # checkov:skip=CKV_AWS_144:the resource is auto generated to be a destination for replication
+  bucket = aws_s3_bucket.data_log_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_iam_role" "data_log_bucket_replication" {
+  name = "aws-iam-role"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "s3.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_s3_bucket_replication_configuration" "data_log_bucket" {
+  depends_on = [aws_s3_bucket_versioning.data_log_bucket]
+  role   = aws_iam_role.data_log_bucket_replication.arn
+  bucket = aws_s3_bucket.data_log_bucket.id
+  rule {
+    id = "foobar"
+    status = "Enabled"
+    destination {
+      bucket        = aws_s3_bucket.data_log_bucket_destination.arn
+      storage_class = "STANDARD"
+    }
+  }
+}
+
+
+
 resource "aws_s3_bucket_logging" "data" {
   bucket = aws_s3_bucket.data.id
 
